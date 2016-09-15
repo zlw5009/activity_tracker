7bb12ac438f4e22ae0adefa070660746273b3858
Zachary Williams <zlw5009@gmail.com>
2016-09-15 01:57:04 -0500

    Added DB functionality and views to add, update, and delete topics

--- database.rb
+++ database.rb
@@ -17,17 +17,49 @@ class Database
 
   def all_subjects
     sql = "SELECT * FROM subjects;"
-
     result = query(sql)
 
-    result.map { |tuple| tuple_to_hash(tuple) }
+    result.map { |tuple| tuple_subject_to_hash(tuple) }
+  end
+
+  def subject_info(subject_id)
+    sql = "SELECT * FROM subjects WHERE id = $1"
+    result = query(sql, subject_id)
+
+    result.map { |tuple| tuple_subject_to_hash(tuple) }    
+  end
+
+  def all_topics(subject_id)
+    sql = "SELECT * FROM topics WHERE subject_id = $1"
+    result = query(sql, subject_id)
+
+    result.map { |tuple| tuple_topic_to_hash(tuple) }
+  end
+
+  def create_new_topic(topic_name, subject_id)
+    sql = "INSERT INTO topics (name, subject_id) VALUES ($1, $2)"
+
+    query(sql, topic_name, subject_id)
+  end
+
+  def delete_topic(topic_id)
+    sql = "DELETE FROM topics WHERE id = $1"
+
+    query(sql, topic_id)
   end
 
   private 
 
-  def tuple_to_hash(tuple)
+  def tuple_subject_to_hash(tuple)
     { id: tuple["id"].to_i,
       name: tuple["name"],
       duration: tuple["duration"] }
+  end
+
+  def tuple_topic_to_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      start_time: tuple["start_time"],
+      end_time: tuple["end_time"] }
   end
 end
